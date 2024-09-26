4b8b0d3452e37846f0211214e0c255abc00ae526
佟祥正 <13956394+tong-xiangzheng@user.noreply.gitee.com>
2024-09-26 17:46:47 +0800

    update

---
+++ test/testbin.py
@@ -0,0 +1,5 @@
+import autotest_binary
+import autotest_src
+
+autotest_binary.autotest_binary("apt-config-icons-hidpi","0.15.2","2")
+#autotest_src.autotest_src("appstream","apt-config-icons-hidpi","0.15.2","2")
---
+++ test/testsrc.py
@@ -0,0 +1,5 @@
+import autotest_binary
+import autotest_src
+
+#autotest_binary.autotest_binary("busybox-initramfs","1.30.1","7ubuntu3")
+autotest_src.autotest_src("appstream","apt-config-icons-hidpi","0.15.2","2")
